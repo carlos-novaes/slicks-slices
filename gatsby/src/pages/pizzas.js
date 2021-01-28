8e4c66711c25a739001f5c72619abcdf0685eb04
Carlos Novaes <carlos.novaes@elevaeducacao.com.br>
2021-01-27 21:33:07 -0300

    criando arquivo layout para aplicação

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 export default function PizzasPage() {
-  return <p>Hey! I'm the pizzas page</p>;
+  return (
+    <Layout>
+      <p>Hey! I'm the pizzas page</p>
+    </Layout>
+  );
 }
